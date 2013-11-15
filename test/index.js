0d7d76759770ce6b6b766417ee5ba68587762be6
Matt-Esch <matt@mattesch.info>
2013-11-14 22:04:25 -0800

    added test for local file

--- test/index.js
+++ test/index.js
@@ -1,8 +1,24 @@
+
+var token = "9rhtgbolui4yrncqowuy;y45b"
+
 var test = require("tape")
 
-var peerModule = require("../index")
+var cpeer = require("../index")({
+    peerDirectory: __dirname
+})
+
+
+module.exports = token
+
+
 
 test("peerModule is a function", function (assert) {
-    assert.equal(typeof peerModule, "function")
+    assert.equal(typeof cpeer, "function")
     assert.end()
 })
+
+test("require peer in current directory", function (assert) {
+    assert.equal(cpeer(""), token)
+    assert.end()
+})
+
